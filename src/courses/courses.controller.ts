d0caa46026ec2c4af58027fb1bf06b8fabcbe792
Pedro Henrique Serôdio de Oliveira <serodiomg@gmail.com>
2023-03-30 10:11:03 -0300

    feat: Parametros dinamicos na rota

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,15 @@
 import { Controller } from '@nestjs/common';
-import { Get } from '@nestjs/common/decorators';
+import { Get, Param } from '@nestjs/common/decorators';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
   findAll(): string {
     return 'Listagem de cursos';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): string {
+    return `Curso #${id}`;
   }
 }
