5817df6bff4514ff3fbd6556519261615f34c183
Pedro Henrique Serôdio de Oliveira <serodiomg@gmail.com>
2023-03-30 10:36:23 -0300

    feat: :art: Customizando o Status code das respostas

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,11 +1,18 @@
-import { Controller } from '@nestjs/common';
-import { Get, Param, Post, Body } from '@nestjs/common/decorators';
+import { Controller, HttpStatus } from '@nestjs/common';
+import {
+  Get,
+  Param,
+  Post,
+  Body,
+  HttpCode,
+  Res,
+} from '@nestjs/common/decorators';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
-  findAll(): string {
-    return 'Listagem de cursos';
+  findAll(@Res() res): string {
+    return res.status(200).send('Listagem de cursos');
   }
 
   @Get(':id')
@@ -14,6 +21,7 @@ export class CoursesController {
   }
 
   @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() data: any): any {
     return data;
   }
