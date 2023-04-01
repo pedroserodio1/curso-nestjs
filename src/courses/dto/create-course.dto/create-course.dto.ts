1553d72f3ab55e54aa9c9d4b00d94b81f1d10c44
Pedro Henrique Serôdio de Oliveira <serodiomg@gmail.com>
2023-04-01 18:17:41 -0300

    feat: Validando dados recebidos pelo App

--- src/courses/dto/create-course.dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto/create-course.dto.ts
@@ -1,5 +1,14 @@
+import { IsString } from 'class-validator';
+
 export class CreateCourseDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsString({
+    each: true,
+  })
   readonly tags: string[];
 }
--- src/courses/dto/update-course.dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto/update-course.dto.ts
@@ -1,5 +1,5 @@
-export class UpdateCourseDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly tags?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCourseDto } from '../create-course.dto/create-course.dto';
+
+//partial types permite usar os tipos da class createCourseDto sem a obrigatoriedade dos dados dela, permitindo enviar apenas um ou todos. Importa tambem as regras de validação do class validator
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
